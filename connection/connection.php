a9eeb5f290bc094e0117691b54b7f124835db5b8
Md Sadiqul Islam Khan <46827272+sadik491@users.noreply.github.com>
2019-05-04 23:36:12 +0600

    Create connection.php

---
+++ connection/connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "id5506355_travelbd";
+$password = "$$travelBD%%";
+$database = "id5506355_travelbangladesh";
+
+try {
+    $conn = new PDO("mysql:host=$localhost;dbname=$id5506355_travelbangladesh", $id5506355_travelbd, $id5506355_travelbangladesh);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    } catch(PDOException $e) {    
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
